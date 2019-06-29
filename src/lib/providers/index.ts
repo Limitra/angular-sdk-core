b1accc63718a9c1857a724aa0761ae908eb699a8
Mustafa Nevfel <mnevfel@limitra.com>
2019-06-29 22:38:29 +0300

    String provider added to singleton.

--- src/lib/providers/index.ts
+++ src/lib/providers/index.ts
@@ -1,13 +1,16 @@
 import {Http} from './http';
 import {HttpClient} from '@angular/common/http';
-import {Inject} from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
 import {StorageProvider} from './storage';
 import {UrlProvider} from './url';
+import {StringProvider} from './string';
 
+@Injectable()
 export class SdkProviders {
   constructor(@Inject(HttpClient) public http: HttpClient) {}
 
   Storage = new StorageProvider();
   Http = new Http(this.http, this.Storage);
   Url = new UrlProvider();
+  String = new StringProvider();
 }
