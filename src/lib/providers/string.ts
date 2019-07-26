e6321a9c1763138a044a6694d3569633df429387
Mustafa Nevfel <mnevfel@limitra.com>
2019-07-26 05:14:00 +0300

    Random string generator function added.

--- src/lib/providers/string.ts
+++ src/lib/providers/string.ts
@@ -2,4 +2,14 @@ export class StringProvider {
   Replace(value: string, search: string, replacement: string): string {
     return value.split(search).join(replacement);
   }
+
+  Generate(length: number) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
 }
