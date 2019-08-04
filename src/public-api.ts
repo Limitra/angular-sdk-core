6c2ea2d7ebc7fe51562d29af2709a41efe48cee7
mnevfel <mnevfel@limitra.com>
2019-08-04 21:26:10 +0300

    Request to json result function added to source.

--- src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of sdk-core
- */
-
-export * from './lib/sdk-core.module';
-export * from './lib/providers/index';
-export * from './lib/guards/route-guard';
-export * from './lib/definitions/screen-size';
