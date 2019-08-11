203118c91bf54206e20232d8808350cbef9f1bfd
mnevfel <mnevfel@limitra.com>
2019-08-11 10:02:14 +0300

    Http headers ugpraded for localization and security optimization.

--- src/lib/providers/http.ts
+++ src/lib/providers/http.ts
@@ -4,12 +4,14 @@ import {Observable, throwError} from 'rxjs';
 import {StorageProvider} from './storage';
 
 export class Http {
+  private localization: any;
   private texts: any;
-  constructor(private http: HttpClient, private storage: StorageProvider) {
-   const lang = this.storage.Get('Localization_Settings', '');
 
-    if (lang) {
-      this.http.get('assets/locale/interface/' + lang + '.json').subscribe(response => {
+  constructor(private http: HttpClient, private storage: StorageProvider) {
+    this.localization = this.storage.Get('Localization_Settings') || {};
+
+    if (this.localization.Language) {
+      this.http.get('assets/locale/interface/' + this.localization.Language + '.json').subscribe(response => {
         this.texts = response;
       });
     } else {
@@ -24,46 +26,60 @@ export class Http {
 
   Get(url: string, error: (HttpErrorResponse) => void = null): Observable<any> {
     return this.http.get(url, this.headers())
-        .pipe(catchError((response) => { return this.handleError(response, error); }));
+      .pipe(catchError((response) => {
+        return this.handleError(response, error);
+      }));
   }
 
   Post(url: string, data: any, error: (HttpErrorResponse) => void = null): Observable<any> {
     return this.http.post(url, data, this.headers())
-        .pipe(catchError((response) => { return this.handleError(response, error); }));
+      .pipe(catchError((response) => {
+        return this.handleError(response, error);
+      }));
   }
 
   Put(url: string, data: any, error: (HttpErrorResponse) => void = null): Observable<any> {
     return this.http.put(url, data, this.headers())
-        .pipe(catchError((response) => { return this.handleError(response, error); }));
+      .pipe(catchError((response) => {
+        return this.handleError(response, error);
+      }));
   }
 
   Delete(url: string, error: (HttpErrorResponse) => void = null): Observable<any> {
     return this.http.delete(url, this.headers())
-      .pipe(catchError((response) => { return this.handleError(response, error); }));
+      .pipe(catchError((response) => {
+        return this.handleError(response, error);
+      }));
   }
 
   private headers() {
-    const jwt = this.storage.Get('Authorization');
-    return jwt ? {
-      headers: new HttpHeaders({
-        Authorization: jwt,
-        Language: '',
-        TimeZone: ''
-      })
-    } : {};
+    const jwt = this.storage.Get('Authentication_Settings');
+    const headers: any = {};
+    if (jwt && jwt.Prefix && jwt.Token) {
+      headers.Authorization = jwt.Prefix + ' ' + jwt.Token;
+    }
+    if (this.localization.Language) {
+      headers.Language = this.localization.Language;
+    }
+    if (this.localization.TimeZone) {
+      headers.TimeZone = this.localization.TimeZone;
+    }
+    return {
+      headers: new HttpHeaders(headers)
+    };
   }
 
   private handleError(error: HttpErrorResponse, callback: (HttpErrorResponse) => void) {
     if (error.error instanceof ErrorEvent) {
       // A client-side or network error occurred. Handle it accordingly.
       if (callback) {
-        callback({ status: '4**', message: this.texts.ErrorClient, detail: error.error.message })
+        callback({status: '4**', message: this.texts.ErrorClient, detail: error.error.message});
       }
     } else {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong,
       if (callback) {
-        callback({ status: error.status || '5**', message: this.texts.ErrorServer, response: error.error });
+        callback({status: error.status || '5**', message: this.texts.ErrorServer, response: error.error});
       }
     }
     // return an observable with a user-facing error message
