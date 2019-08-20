bf856ab987b546eee610531d4fc07de2c0a8d51b
mnevfel <mnevfel@limitra.com>
2019-08-20 18:37:54 +0300

    Bind provider App method generated.

--- src/lib/providers/bind.ts
+++ src/lib/providers/bind.ts
@@ -1,5 +1,33 @@
+import {isDevMode} from "@angular/core";
+
 export class BindProvider {
   Track(index: number, obj: any): any {
     return index;
+  }
+
+  App() {
+    if (!isDevMode()) {
+      const logger = function nLog() {
+        let oldConsoleLog = null;
+        const pub: any = {};
+
+        pub.enableLogger = function enableLogger() {
+          if (oldConsoleLog == null) {
+            return;
+          }
+
+          window.console.log = oldConsoleLog;
+        };
+
+        pub.disableLogger = function disableLogger() {
+          oldConsoleLog = console.log;
+          window.console.log = function () {
+          };
+        };
+
+        return pub;
+      }();
+      logger.disableLogger();
+    }
   }
 }
