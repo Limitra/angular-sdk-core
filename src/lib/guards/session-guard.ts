8cadb3096b921b0720e42bbc39cca45a1b08e0a9
mnevfel <mnevfel@limitra.com>
2019-09-05 20:31:51 +0300

    Session guard activated for parent routes.

--- src/lib/guards/session-guard.ts
+++ src/lib/guards/session-guard.ts
@@ -1,15 +1,23 @@
 import {Injectable} from '@angular/core';
-import { CanActivateChild } from '@angular/router';
+import {CanActivate, CanActivateChild} from '@angular/router';
 import {SdkProviders} from '../providers';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SessionGuard implements CanActivateChild {
+export class SessionGuard implements CanActivateChild, CanActivate {
   constructor(private providers: SdkProviders) {
   }
 
   canActivateChild(): boolean {
+    const jwt = this.providers.Storage.Get('Authentication_Settings');
+    if (jwt && jwt.Login && !jwt.Token) {
+      this.providers.Router.Navigate(jwt.Login);
+    }
+    return jwt && jwt.Token ? true : false;
+  }
+
+  canActivate(): boolean {
     const jwt = this.providers.Storage.Get('Authentication_Settings');
     if (jwt && jwt.Login && !jwt.Token) {
       this.providers.Router.Navigate(jwt.Login);
