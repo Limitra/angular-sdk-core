cc75e559c3888141b543ae60f02d7b360075e594
Mustafa Nevfel <mnevfel@limitra.com>
2020-05-08 17:13:47 +0300

    SecurityKey enabled for grecaptcha.

--- src/lib/providers/http.ts
+++ src/lib/providers/http.ts
@@ -1,16 +1,19 @@
 import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
-import {catchError} from 'rxjs/operators';
-import {Observable, throwError} from 'rxjs';
+import {catchError, mergeMap} from 'rxjs/operators';
+import {Observable, throwError, from} from 'rxjs';
 import {StorageProvider} from './storage';
 import {RouterProvider} from './router';
 import {DeviceProvider} from './device';
+import {StringProvider} from './string';
+
+declare let grecaptcha;
 
 export class Http {
   private localization: any;
   private texts: any;
 
   constructor(private http: HttpClient, private router: RouterProvider, private storage: StorageProvider,
-              private device: DeviceProvider) {
+              private str: StringProvider, private device: DeviceProvider) {
     this.localization = this.storage.Get('Localization_Settings') || {};
 
     if (this.localization.Language) {
@@ -28,41 +31,51 @@ export class Http {
   }
 
   File(url: string, error: (HttpErrorResponse) => void = null): Observable<any> {
-    const headers: any = this.headers();
-    headers.responseType = 'arraybuffer';
-    return this.http.get(url, headers)
-      .pipe(catchError((response) => {
-        return this.handleError(response, error);
-      }));
+    return this.headers(url).pipe(mergeMap((headers: any) => {
+      headers.responseType = 'arraybuffer';
+      return this.http.get(url, headers)
+        .pipe(catchError((response) => {
+          return this.handleError(response, error);
+        }));
+    }));
   }
 
   Get(url: string, error: (HttpErrorResponse) => void = null): Observable<any> {
-    return this.http.get(url, this.headers())
-      .pipe(catchError((response) => {
-        return this.handleError(response, error);
-      }));
+    return this.headers(url).pipe(mergeMap((headers: any) => {
+      return this.http.get(url, headers)
+        .pipe(catchError((response) => {
+          return this.handleError(response, error);
+        }));
+    }));
   }
 
   Post(url: string, data: any, error: (HttpErrorResponse) => void = null): Observable<any> {
-    return this.http.post(url, data, this.headers())
-      .pipe(catchError((response) => {
-        return this.handleError(response, error);
-      }));
+    return this.headers(url).pipe(mergeMap((headers: any) => {
+      return this.http.post(url, data, headers)
+        .pipe(catchError((response) => {
+          return this.handleError(response, error);
+        }));
+    }));
   }
 
   Put(url: string, data: any, error: (HttpErrorResponse) => void = null): Observable<any> {
-    return this.http.put(url, data, this.headers())
-      .pipe(catchError((response) => {
-        return this.handleError(response, error);
-      }));
+    return this.headers(url).pipe(mergeMap((headers: any) => {
+      return this.http.put(url, data, headers)
+        .pipe(catchError((response) => {
+          return this.handleError(response, error);
+        }));
+    }));
   }
 
   Delete(url: string, error: (HttpErrorResponse) => void = null): Observable<any> {
-    return this.http.delete(url, this.headers())
-      .pipe(catchError((response) => {
-        return this.handleError(response, error);
-      }));
+    return this.headers(url).pipe(mergeMap((headers: any) => {
+      return this.http.delete(url, headers)
+        .pipe(catchError((response) => {
+          return this.handleError(response, error);
+        }));
+    }));
   }
+
 
   Initialize() {
     const jwt = this.storage.Get('Authentication_Settings');
@@ -78,25 +91,54 @@ export class Http {
     return this.storage.Get('Authentication_Settings');
   }
 
-  private headers() {
-    const jwt = this.Initialize();
+  private headers(url: string) {
+    const promise = new Promise((resolve, reject) => {
+      const jwt = this.Initialize();
 
-    const headers: any = {
-      Device: this.device.Get()
-    };
-    if (jwt && jwt.Token) {
-      headers.Authorization = jwt.Token;
-    }
-    if (this.localization.Language) {
-      headers.Language = this.localization.Language;
-    }
-    if (this.localization.TimeZone || this.localization.TimeZone === 0) {
-      headers.TimeZone = this.localization.TimeZone.toString();
-    }
+      const headers: any = this.device.Get();
+      if (jwt && jwt.Token) {
+        headers.Authorization = jwt.Token;
+      }
+      if (this.localization.Language) {
+        headers.Language = this.localization.Language;
+      }
+      if (this.localization.TimeZone || this.localization.TimeZone === 0) {
+        headers.TimeZone = this.localization.TimeZone.toString();
+      }
 
-    return {
-      headers: new HttpHeaders(headers)
-    };
+      if (jwt && jwt.SecurityKey) {
+        let method = url.includes('?') ? url.split('?')[0] : url;
+        method = url.includes('&') ? url.split('&')[0] : url;
+        method = this.str.Replace(method, '/', '');
+        method = this.str.Replace(method, ':', '');
+        method = this.str.Replace(method, '.', '');
+        method = this.str.Replace(method, '-', '');
+        method = this.str.Replace(method, 'http', '');
+        method = this.str.Replace(method, 'https', '');
+        try {
+          grecaptcha.execute(jwt.SecurityKey, { action: method })
+            .then((token) => {
+              headers.SecurityToken = token;
+              resolve({
+                headers: new HttpHeaders(headers)
+              });
+            }, () => {
+              resolve({
+                headers: new HttpHeaders(headers)
+              });
+            });
+        } catch {
+          resolve({
+            headers: new HttpHeaders(headers)
+          });
+        }
+      } else {
+        resolve({
+          headers: new HttpHeaders(headers)
+        });
+      }
+    });
+    return from(promise);
   }
 
   private handleError(error: HttpErrorResponse, callback: (HttpErrorResponse) => void) {
